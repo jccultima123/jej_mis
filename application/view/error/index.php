813a2015344e36be049c033914f0d5be9cd93a8e
jccultima123 <johncyrillcorsanes@hotmail.com>
2015-02-19 19:09:46 +0800

    Minor Changes

--- application/view/error/index.php
+++ application/view/error/index.php
@@ -6,7 +6,8 @@
 <div class="container" style="background-color: transparent;">
     <p>
         It seems that the page you've requested does not exist<br />
-        or your request does not completed with errors
+        or your request are not available this time <br /><br />
+        Please contact your Administrator for details.
         <br /><br /><a href="<?php echo URL; ?>" class="back">&larr; Go Back</a>
     </p>
 </div>
