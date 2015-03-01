02699207245e18bb44a976a82ed1de7f934c7e07
jccultima123 <johncyrillcorsanes@hotmail.com>
2015-03-01 14:38:17 +0800

    Transferring to Control Panel page

---
+++ cpanel/application/view/_templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>MOBILIZER</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- JS -->
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+
+    <!-- CSS -->
+    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- logo -->
+    <div class="logo">
+        MOBILIZER
+    </div>
+
+    <!-- navigation -->
+    <div class="navigation">
+        <a href="<?php echo URL; ?>">home</a>
+        <a href="<?php echo URL; ?>products">products</a>
+        <a href="<?php echo URL; ?>services">services</a>
+        <!-- <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a> -->
+        <a href="<?php echo URL; ?>about">about us</a>
+        
+        <br /><br />
+        <div id="login-form-mini">
+            Login not available at the moment
+        </div>
+    </div>
---
+++ cpanel/application/view/error/index.php
@@ -0,0 +1,13 @@
+<style>
+    html {background-color: gold;}
+    .container {border: 1px solid gold;}
+</style>
+
+<div class="container" style="background-color: transparent;">
+    <p>
+        It seems that the page you've requested does not exist<br />
+        or your request are not available this time <br /><br />
+        Please contact your Administrator for details.
+        <br /><br /><a href="<?php echo URL; ?>" class="back">&larr; Go Back</a>
+    </p>
+</div>
