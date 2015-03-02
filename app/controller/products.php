e9aadc56cbedf7939c8fe719c9d299f2968618a6
jccultima123 <johncyrillcorsanes@hotmail.com>
2015-03-02 08:14:11 +0800

    Minor Changes

---
+++ app/controller/products.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+class Products extends Controller
+{
+    
+    /**
+     * PAGE: index
+     * This method handles what happens when you move to http://yourproject/---/index
+     */
+    public function index()
+    {
+        // getting all songs and amount of songs
+        $products = $this->model->getAllProducts();
+        $amount_of_products = $this->model->getAmountOfProducts();
+
+       // load views. within the views we can echo out $songs and $amount_of_songs easily
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/products/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+    
+    public function addProduct()
+    {
+        // if we have POST data to create a new song entry
+        if (isset($_POST["submit_add_product"])) {
+            // ADD THIS in model/model.php
+            $this->model->addProduct($_POST["category"], $_POST["SKU"], $_POST["product_name"], $_POST["product_model"], $_POST["manufacturer_name"], $_POST["price"], $_POST["link"]);
+        }
+
+        // where to go after song has been added
+        header('location: ' . URL . 'products/index');
+    }
+    
+    public function deleteProduct($product_id)
+    {
+        // if we have an id of a song that should be deleted
+        if (isset($product_id)) {
+            // do deleteSong() in model/model.php
+            $this->model->deleteSong($product_id);
+        }
+
+        // where to go after song has been deleted
+        header('location: ' . URL . 'songs/index');
+    }
+
+    public function editProduct($product_id)
+    {
+        // if we have an id of a song that should be edited
+        if (isset($product_id)) {
+            // do getProduct() in model/model.php
+            $tb_product = $this->model->getProduct($product_id);
+
+            // in a real application we would also check if this db entry exists and therefore show the result or
+            // redirect the user to an error page or similar
+
+            // load views. within the views we can echo out $song easily
+            require APP . 'view/_templates/header.php';
+            require APP . 'view/products/edit.php';
+            require APP . 'view/_templates/footer.php';
+        } else {
+            // redirect user to songs index page (as we don't have a song_id)
+            header('location: ' . URL . 'products/index');
+            $error = '<h4>Unable to edit.</h4>';
+        }
+    }
+
+}
---
+++ app/model/model.php
@@ -0,0 +1,207 @@
+<?php
+
+class Model
+{
+    /**
+     * @param object $db A PDO database connection
+     */
+    function __construct($db)
+    {
+        try {
+            $this->db = $db;
+        } catch (PDOException $e) {
+            exit('Database connection could not be established.');
+        }
+    }
+
+    /**
+     * Get all songs from database
+     */
+    public function getAllSongs()
+    {
+        $sql = "SELECT id, artist, track, link FROM song";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+    
+    /**
+     * Get all products and categories
+     */
+    public function getAllProducts()
+    {
+        $sql = "SELECT product_id, category, SKU, product_name, product_model, manufacturer_name, release_date, price, link FROM tb_products";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        
+        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
+        // core/controller.php! If you prefer to get an associative array as the result, then do
+        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
+        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
+        return $query->fetchAll();
+    }
+    
+    public function getCategories() {
+        $sql = "SELECT * FROM tb_categories";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        
+        return $query->fetchAll();
+    }
+
+    /**
+     * Add a song to database
+     * TODO put this explanation into readme and remove it from here
+     * Please note that it's not necessary to "clean" our input in any way. With PDO all input is escaped properly
+     * automatically. We also don't use strip_tags() etc. here so we keep the input 100% original (so it's possible
+     * to save HTML and JS to the database, which is a valid use case). Data will only be cleaned when putting it out
+     * in the views (see the views for more info).
+     * @param string $artist Artist
+     * @param string $track Track
+     * @param string $link Link
+     */
+    public function addSong($artist, $track, $link)
+    {
+        $sql = "INSERT INTO song (artist, track, link) VALUES (:artist, :track, :link)";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':artist' => $artist, ':track' => $track, ':link' => $link);
+
+        // useful for debugging: you can see the SQL behind above construction by using:
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+
+        $query->execute($parameters);
+    }
+    
+    public function addProduct($category, $SKU, $product_name, $product_model, $manufacturer_name, $price, $link)
+    {
+        $sql = "INSERT INTO tb_products (category, SKU, product_name, product_model, manufacturer_name, price, link) VALUES (:category, :SKU, :product_name, :product_model, :manufacturer_name, :price, :link)";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':category' => $category, ':SKU' => $SKU, ':product_name' => $product_name, ':product_model' => $product_model, ':manufacturer_name' => $manufacturer_name, ':price' => $price, ':link' => $link);
+
+        // useful for debugging: you can see the SQL behind above construction by using:
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+
+        $query->execute($parameters);
+    }
+
+    /**
+     * Delete a song in the database
+     * Please note: this is just an example! In a real application you would not simply let everybody
+     * add/update/delete stuff!
+     * @param int $song_id Id of song
+     */
+    public function deleteSong($song_id)
+    {
+        $sql = "DELETE FROM song WHERE id = :song_id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':song_id' => $song_id);
+
+        // useful for debugging: you can see the SQL behind above construction by using:
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+
+        $query->execute($parameters);
+    }
+
+    /**
+     * Get a song from database
+     */
+    public function getSong($song_id)
+    {
+        $sql = "SELECT id, artist, track, link FROM song WHERE id = :song_id LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':song_id' => $song_id);
+
+        // useful for debugging: you can see the SQL behind above construction by using:
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+
+        $query->execute($parameters);
+
+        // fetch() is the PDO method that get exactly one result
+        return $query->fetch();
+    }
+    
+    public function getProduct($product_id)
+    {
+        $sql = "SELECT product_id, category, SKU, product_name, product_model, manufacturer_name, release_date, price FROM tb_products WHERE product_id = :product_id LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':product_id' => $product_id);
+
+        // useful for debugging: you can see the SQL behind above construction by using:
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+
+        $query->execute($parameters);
+
+        // fetch() is the PDO method that get exactly one result
+        return $query->fetch();
+    }
+
+    /**
+     * Update a song in database
+     * // TODO put this explaination into readme and remove it from here
+     * Please note that it's not necessary to "clean" our input in any way. With PDO all input is escaped properly
+     * automatically. We also don't use strip_tags() etc. here so we keep the input 100% original (so it's possible
+     * to save HTML and JS to the database, which is a valid use case). Data will only be cleaned when putting it out
+     * in the views (see the views for more info).
+     * @param string $artist Artist
+     * @param string $track Track
+     * @param string $link Link
+     * @param int $song_id Id
+     */
+    public function updateSong($artist, $track, $link, $song_id)
+    {
+        $sql = "UPDATE song SET artist = :artist, track = :track, link = :link WHERE id = :song_id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':artist' => $artist, ':track' => $track, ':link' => $link, ':song_id' => $song_id);
+
+        // useful for debugging: you can see the SQL behind above construction by using:
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+
+        $query->execute($parameters);
+    }
+
+    /**
+     * Get simple "stats". This is just a simple demo to show
+     * how to use more than one model in a controller (see application/controller/songs.php for more)
+     */
+    public function getAmountOfSongs()
+    {
+        $sql = "SELECT COUNT(id) AS amount_of_songs FROM song";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        // fetch() is the PDO method that get exactly one result
+        return $query->fetch()->amount_of_songs;
+    }
+    
+    public function getAmountOfProducts()
+    {
+        $sql = "SELECT COUNT(product_id) AS amount_of_products FROM tb_products";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        // fetch() is the PDO method that get exactly one result
+        return $query->fetch()->amount_of_products;
+    }
+    
+    //GET MYSQL VERSION
+    public function getMySqlVersion()
+    {
+        $sql = "SELECT VERSION()";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        // fetch() is the PDO method that get exactly one result
+        return $query->fetch();
+    }
+    
+    //SORTING RELEASE DATES
+    public function sortDate()
+    {
+        $sql = "SELECT product_id, categor, SKU, product_name, product_model, manufacturer_name, release_date, price, link FROM tb_products ORDER BY release_date";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        
+        return $query->fetchAll();
+    }
+}
