c1238f162bc3e8bac8a2fc83cd45214337f21ee4
jccultima123 <johncyrillcorsanes@hotmail.com>
2015-03-02 08:14:42 +0800

    Minor Changes

---
+++ app/view/_templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>MOBILIZER - Mobilize your way.</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- JS -->
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+
+    <!-- CSS -->
+    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="header-cont">
+        <div id="header">
+            <ul>
+                <li><a href="<?php echo URL; ?>" id="title">MOBILIZER</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="feeds.php">News</a></li>
+                <li><a href="catalog.php">Products</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+        </div>
+    </div>
