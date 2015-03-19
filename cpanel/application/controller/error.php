f03b60328daeb00d2b82f32928f541802ac2e0ba
jccultima123 <johncyrillcorsanes@gmail.com>
2015-03-19 19:00:04 +0800

    Added 401 Error

--- cpanel/application/controller/error.php
+++ cpanel/application/controller/error.php
@@ -21,4 +21,12 @@ class Error extends Controller
         require APP . 'view/error/index.php';
         require APP . 'view/error/error_footer.php';
     }
+    
+    public function accessdenied()
+    {
+        // load views
+        require APP . 'view/error/error_header.php';
+        require APP . 'view/error/401.php';
+        require APP . 'view/error/error_footer.php';
+    }
 }
