6141e59720aead9f9293d6feb55f09abac10fe80
jccultima123 <johncyrillcorsanes@gmail.com>
2015-03-19 19:00:29 +0800

    Minor Error Detect Changes

--- cpanel/application/libs/Auth.php
+++ cpanel/application/libs/Auth.php
@@ -16,7 +16,7 @@ class Auth
         // redirect user to login page
         if (!isset($_SESSION['user_logged_in'])) {
             Session::destroy();
-            header('location: ' . URL . 'login');
+            header('location: ' . URL . 'error/accessdenied');
             // to prevent fetching views via cURL (which "ignores" the header-redirect above) we leave the application
             // the hard way, via exit(). @see https://github.com/panique/php-login/issues/453
             exit();
