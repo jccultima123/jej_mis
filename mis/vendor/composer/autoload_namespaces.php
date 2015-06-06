7b8183b3331823f2137ad9c03b3c0cfb35ffb87d
jccultima123 <johncyrillcorsanes@gmail.com>
2015-06-06 21:27:54 +0800

    Refreshing Composer libraries

--- mis/vendor/composer/autoload_namespaces.php
+++ mis/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPExcel' => array($vendorDir . '/phpoffice/phpexcel/Classes'),
     'Gregwar\\Captcha' => array($vendorDir . '/gregwar/captcha'),
 );
