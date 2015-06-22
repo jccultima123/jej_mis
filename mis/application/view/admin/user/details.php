5f5b82c079ad89a87bcf7381f564cdf18f6e19e5
jccultima123 <johncyrillcorsanes@gmail.com>
2015-06-22 11:40:14 +0800

    Some padding fixes

--- mis/application/view/admin/user/details.php
+++ mis/application/view/admin/user/details.php
@@ -12,69 +12,69 @@
                 <h4>DETAILS</h4>
                 <div class="row">
                     <label class="col-xs-4 control-label">User No.</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->user_id; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">User Name</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->user_name; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">User Authentication Code</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->user_password; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">User Type</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->user_provider_type; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">Designated JEJ Branch</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->branch_name; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">User Name</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->user_name; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">Email Address</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->user_email; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">First Name</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->first_name; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">Last Name</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->last_name; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
                     <label class="col-xs-4 control-label">Middle Name</label>
-                    <span class="col-xs-8">
+                    <p class="col-xs-8">
                         <?php echo $user->middle_name; ?>
-                    </span>
+                    </p>
                 </div>
                 <div class="row">
-                    <span class="col-xs-8 col-xs-offset-4">
+                    <p class="col-xs-8 col-xs-offset-4">
                         <br />
                         <a type="button" class="btn btn-danger" href="<?php echo URL . 'admin/deactivateUser/' . htmlspecialchars($user->user_id, ENT_QUOTES, 'UTF-8'); ?>">Deactivate</a>
-                    </span>
+                    </p>
                 </div>
             </div>
         </div>
