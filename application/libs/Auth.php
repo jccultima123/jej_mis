53ada23b70247b752ba0ec0eafb51cff80dcc8ed
jccultima123 <johncyrillcorsanes@gmail.com>
2015-06-27 18:11:57 +0800

    Minor fixes

--- application/libs/Auth.php
+++ application/libs/Auth.php
@@ -46,6 +46,10 @@ class Auth
             $ERROR = 'SORRY. You are not allowed to use this page. Please logout your current session and';
             require_once '_fb/403.html';
             exit();
+        } else if (isset($_SESSION['user_logged_in'])) {
+            $ERROR = 'SORRY. You are not allowed to use this page. Please logout your current session and';
+            require_once '_fb/403.html';
+            exit();
         }
     }
     
