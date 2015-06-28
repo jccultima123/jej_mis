f2890034e0bc6b823cf7ab9d96ebd6f9a33ded2d
jccultima123 <johncyrillcorsanes@gmail.com>
2015-06-28 18:48:00 +0800

    Some furnishments.

---
+++ application/view/_users/header/admin_logout.php
@@ -0,0 +1,3 @@
+<a id="load" href="<?php echo URL ?>admin" class="navbar-brand" type="button" aria-expanded="false">
+                    <span id="logo">JEJ // MOBILIZER</span>
+                </a>
