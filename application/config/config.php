3a1d3b8e307f8f1457aece3d5891e1ebe9059ad4
jccultima123 <johncyrillcorsanes@gmail.com>
2015-07-03 05:59:04 +0800

    Added Messages for ORDERS

--- application/config/config.php
+++ application/config/config.php
@@ -333,6 +333,11 @@ define("FEEDBACK_CONTACT_ADMINISTRATOR", 'The email server might be offline at t
 define("FEEDBACK_USER_CLEANED", "Some Unfinished Registrations was been cleaned.");
 define("FEEDBACK_ADMIN_ERROR", "Username could not be changed.");
 
+/** ORDER **/
+define("FEEDBACK_ORDER_ACCEPTED", "ORDER ACCEPTED!");
+define("FEEDBACK_ORDER_REJECTED", "You've just rejected the ORDER!");
+define("FEEDBACK_ORDER_FAILED", "Unable to process the ORDER. Please try again.");
+
 /** CRUD MESSAGES **/
 define("CRUD_ADDED", "Added.");
 define("CRUD_UPDATED", "Updated.");
