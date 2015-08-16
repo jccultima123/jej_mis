49952e14004725ce2785a62d24f469448306e3d1
jccultima123 <johncyrillcorsanes@gmail.com>
2015-08-16 17:27:01 +0800

    Performance Improvements (Based from web results)

--- application/view/export/footer.php
+++ application/view/export/footer.php
@@ -1,16 +1,13 @@
 
+    <!-- OTHERS -->
+    <script src="<?php echo URL; ?>assets_new/js/ajax.js" type="text/javascript"></script>
+    <script src="<?php echo URL; ?>assets_new/js/application.js" type="text/javascript"></script>
     <!-- EXPORT -->
     <script type="text/javascript" src="<?php echo URL; ?>assets_new/js/FileSaver/FileSaver.min.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>assets_new/js/html2canvas/html2canvas.min.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>assets_new/js/jsPDF/jspdf.min.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>assets_new/js/jsPDF-AutoTable/jspdf.plugin.autotable.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>assets_new/js/tableExport.min.js"></script>
-    <!-- OTHERS -->
-    <script src="<?php echo URL; ?>assets_new/js/ajax.js" type="text/javascript"></script>
-    <script src="<?php echo URL; ?>assets_new/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="<?php echo URL; ?>assets_new/js/DataTables.bootstrap.js" type="text/javascript"></script>
-    <script src="<?php echo URL; ?>assets_new/js/bootstrap-select.min.js" type="text/javascript"></script>
-    <script src="<?php echo URL; ?>assets_new/js/animate.js" type="text/javascript"></script>
     
     <?php require VIEWS_PATH . '_script/datatables.php'; ?>
     
