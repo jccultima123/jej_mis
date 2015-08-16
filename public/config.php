aa4861ec2960affae08df35215548f1565e9eb0e
jccultima123 <johncyrillcorsanes@gmail.com>
2015-08-16 17:55:26 +0800

    Revert some configs

--- public/config.php
+++ public/config.php
@@ -27,7 +27,7 @@
      *  -   define('CHECK_URL', 'your url');
      *      URL to test Internet Connection for sending mails
      */
-        $env = 'dev_web';
+        $env = 'development';
         
     /*
      * View Configurations
