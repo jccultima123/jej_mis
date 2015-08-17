f1bf0d079529e128fe65a54bec1f0818e7772d93
jccultima123 <johncyrillcorsanes@gmail.com>
2015-08-17 10:28:03 +0800

    Oops

--- application/view/admin/home/index.php
+++ application/view/admin/home/index.php
@@ -17,7 +17,7 @@
                                 <a id="load" class="list-group-item" href="<?php echo URL; ?>AMS"><i class="picol_document_sans"></i>Assets<?php if ($asset_count > 0) { ?> <span class="badge pull-right"><?php echo number_format($asset_count); ?></span> <?php } ?></a>
                                 <a id="load" class="list-group-item" href="<?php echo URL; ?>CRM/customers"><i class="picol_user_full"></i>Customers <?php if ($amount_of_customers > 0) { ?> <span class="badge pull-right"><?php echo number_format($amount_of_customers); ?></span> <?php } ?></a>
                                 <a id="load" class="list-group-item" href="<?php echo URL; ?>CRM/feedbacks"><i class="glyphicon glyphicon-comment"></i> Feedbacks <?php if ($feedback_count > 0) { ?> <span class="badge pull-right"><?php echo number_format($feedback_count);  ?></span> <?php } ?></a>
-                                <a id="load" class="list-group-item" href="<?php echo URL; ?>CRM/feedbacks"><i class="glyphicon glyphicon-list"></i> Audit Logs <?php if ($audit_count > 0) { ?> <span class="badge pull-right"><?php echo number_format($audit_count);  ?></span> <?php } ?></a>
+                                <a id="load" class="list-group-item" href="<?php echo URL; ?>admin/audit"><i class="glyphicon glyphicon-list"></i> Audit Logs <?php if ($audit_count > 0) { ?> <span class="badge pull-right"><?php echo number_format($audit_count);  ?></span> <?php } ?></a>
                             </div>
                         </div>
                         <div class="col-md-3">
