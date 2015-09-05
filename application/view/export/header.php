d546daac9bb8ca0fc386b1678b04365853fd4c17
jccultima123 <johncyrillcorsanes@gmail.com>
2015-09-05 17:01:58 +0800

    UI minor changes

--- application/view/export/header.php
+++ application/view/export/header.php
@@ -67,7 +67,7 @@
                 <div class="modal-body">
                     <h4>Generating Data..</h4>
                     <div class="progress progress-popup">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+                        <div class="progress-bar progress-bar-striped progress-bar-danger active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
                     </div>
                 </div>
             </div>
