4d76ce9ec57748352d6217301efb151f3c62f66f
jccultima123 <johncyrillcorsanes@gmail.com>
2015-09-06 15:57:39 +0800

    Preferences : UI fixes part 2

--- application/view/admin/preferences/index.php
+++ application/view/admin/preferences/index.php
@@ -7,18 +7,33 @@
         <div class="panel-body">
             <div class="tabs-left col-md-2">
                 <ul class="nav nav-tabs">
-                    <li class="active"><a>Main</a></li>
-                    <li>
-                        <a id="load" href="<?php echo URL . 'admin/preferences/profile' ?>">Profile</a>
+                    <li class="active">
+                        <a>Dashboard</a>
                     </li>
                     <li>
-                        <a id="load" href="<?php echo URL . 'admin/preferences/users' ?>">Manage Users</a>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/profile' ?>">Admin's Profile</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/appearance' ?>">Appearance</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/users' ?>">Users</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/branches' ?>">Branches</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/recovery' ?>">Recovery</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/recovery' ?>">Troubleshooting</a>
                     </li>
                 </ul>
-
             </div>
             <div class="tab-content col-md-10">
-
+                <div class="alert alert-info">
+                    Not yet available
+                </div>
             </div>
         </div>
         <div class="panel-footer"></div>
--- application/view/admin/preferences/users.php
+++ application/view/admin/preferences/users.php
@@ -5,47 +5,72 @@
             <?php $this->renderFeedbackMessages(); ?>
         </div>
         <div class="panel-body">
-            <a id="load" class="btn btn-default" href="<?php echo URL; ?>admin/userRegister"><span class="glyphicon glyphicon-plus"></span>&nbsp;Add / Register</a>
-            <br/><br/>
-            <table class="table table-striped" id="users">
-                <thead style="font-weight: bold;">
-                <tr>
-                    <th style="cursor: pointer;">USER ID</th>
-                    <th style="cursor: pointer;">USER TYPE</th>
-                    <th style="cursor: pointer;">USERNAME</th>
-                    <th style="cursor: pointer;">BRANCH</th>
-                    <th style="cursor: pointer;">STATUS</th>
-                    <th style="cursor: pointer;">REGISTERED</th>
-                    <th class="sorttable_nosort"></th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($users as $user) { ?>
+            <div class="tabs-left col-md-2">
+                <ul class="nav nav-tabs">
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/main' ?>">Dashboard</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/profile' ?>">Admin's Profile</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/appearance' ?>">Appearance</a>
+                    </li>
+                    <li class="active">
+                        <a>Users</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/branches' ?>">Branches</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/recovery' ?>">Recovery</a>
+                    </li>
+                    <li>
+                        <a id="load" href="<?php echo URL . 'admin/preferences/recovery' ?>">Troubleshooting</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="tab-content col-md-10">
+                <a id="load" class="btn btn-default" href="<?php echo URL; ?>admin/userRegister"><span class="glyphicon glyphicon-plus"></span>&nbsp;Add / Register</a>
+                <br/><br/>
+                <table class="table table-striped" id="users">
+                    <thead style="font-weight: bold;">
                     <tr>
-                        <td><?php if (isset($user->user_id)) echo htmlspecialchars($user->user_id, ENT_QUOTES, 'UTF-8'); ?></td>
-                        <td><?php if (isset($user->user_provider_type)) echo htmlspecialchars($user->user_provider_type, ENT_QUOTES, 'UTF-8'); ?></td>
-                        <td><?php if (isset($user->user_name)) echo htmlspecialchars($user->user_name, ENT_QUOTES, 'UTF-8'); ?></td>
-                        <td><?php if (isset($user->branch_name)) echo htmlspecialchars($user->branch_name, ENT_QUOTES, 'UTF-8'); ?></td>
-                        <td>
-                            <?php if ($user->user_active == 1) { ?>
-                                ACTIVATED
-                            <?php } else { ?>
-                                DISABLED
-                            <?php } ?>
-                        </td>
-                        <td><?php if (isset($user->user_creation_timestamp)) echo date(DATE_CUSTOM, $user->user_creation_timestamp); ?></td>
-                        <td>
-                            <?php if (isset($user->user_id)) { ?>
-                                <a id="load"
-                                   href="<?php echo URL . 'admin/userDetails/' . htmlspecialchars($user->user_id, ENT_QUOTES, 'UTF-8'); ?>">DETAILS</a>
-                            <?php } ?>
-                        </td>
+                        <th style="cursor: pointer;">ID</th>
+                        <th style="cursor: pointer;">TYPE</th>
+                        <th style="cursor: pointer;">USERNAME</th>
+                        <th style="cursor: pointer;">BRANCH</th>
+                        <th style="cursor: pointer;">STATUS</th>
+                        <th style="cursor: pointer;">REGISTERED</th>
+                        <th class="sorttable_nosort"></th>
                     </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-
-
+                    </thead>
+                    <tbody>
+                    <?php foreach ($users as $user) { ?>
+                        <tr>
+                            <td><?php if (isset($user->user_id)) echo htmlspecialchars($user->user_id, ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php if (isset($user->user_provider_type)) echo htmlspecialchars($user->user_provider_type, ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php if (isset($user->user_name)) echo htmlspecialchars($user->user_name, ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php if (isset($user->branch_name)) echo htmlspecialchars($user->branch_name, ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td>
+                                <?php if ($user->user_active == 1) { ?>
+                                    ACTIVATED
+                                <?php } else { ?>
+                                    DISABLED
+                                <?php } ?>
+                            </td>
+                            <td><?php if (isset($user->user_creation_timestamp)) echo date(DATE_CUSTOM, $user->user_creation_timestamp); ?></td>
+                            <td>
+                                <?php if (isset($user->user_id)) { ?>
+                                    <a id="load"
+                                       href="<?php echo URL . 'admin/userDetails/' . htmlspecialchars($user->user_id, ENT_QUOTES, 'UTF-8'); ?>">DETAILS</a>
+                                <?php } ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="panel-footer"></div>
     </div>
